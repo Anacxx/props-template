27dd95a2479e5328b5001dd7ad2fef739d2f4cc9
Anacxx <anacarolina.clcx@gmail.com>
2023-05-06 14:40:43 -0300

    fsd

--- src/Componentes/Carro.js
+++ src/Componentes/Carro.js
@@ -1,16 +1,14 @@
+import React from "react";
 function Carro(props) {
-  console.log("Dentro do carro", props);
   return (
     <div>
-      {/* Exercício 3 - Acessando o carro recebido por props da Garagem */}
       <h2>{props.carro.nome}</h2>
       <ul>
-        <li>Cor: {props.carro.cor} </li>
-        <li>Ano: {props.carro.ano} </li>
-        <li>Flex: {props.carro.flex}</li>
+        <li>Cor:{props.carro.cor}</li>
+        <li>Ano:{props.carro.ano}</li>
+        <li>Flex:{props.carro.flex}</li>
       </ul>
     </div>
   );
 }
-
 export default Carro;
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,24 +1,16 @@
+import React from "react";
+
 import Carro from "./Carro";
-// exercício 1.2 - Altere o componente Garagem para que agora seu nome seja recebido por props, ao invés de ser escrito diretamente no elemento JSX.
 
 function Garagem(props) {
   console.log("Na garagem", props);
   return (
     <div>
-      <h1>Garagem de {props.nomeGaragem}</h1>
-
-      {/* Exercício 2.3 - Adicione no componente Garagem, um elemento de botão. Iremos adicionar um evento de clique no botão no próximo passo; */}
-      <button
-        // Exercício 2.4 - Adicione a chamada da função ao evento de clique no botão que foi criado.
-        onClick={() => props.apresentaGaragem(props.nomeGaragem)}
-      >
-        Apresentar a garagem
-      </button>
-      {/* Exercício 3 - passando cada carro recebido pela Garagem para dentro do componente Carro por props */}
-      <Carro carro={props.carro} />
-      <Carro carro={props.carro2} />
-      <Carro carro={props.carro3} />
-      <Carro carro={props.carro4} />
+      <h1>Garagem de {props.nomeDaGaragem}</h1>
+      <button onClick={() => props.apresentaGaragem(props.nomeDaGaragem)}>Boas vindas</button>
+      <Carro carro = {props.carro}/>
+      <Carro carro = {props.carro2} />
+      <Carro carro = {props.carro3} />
     </div>
   );
 }
